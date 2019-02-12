507ac15d6e7bbac1f4a4588cfa5eeab4f1955b8e
Jason <alvarezcoraiza@citypolyhs.org>
2019-02-12 18:16:54 +0000

    teacher verification functionality is working

--- teacher.js
+++ teacher.js
@@ -4,32 +4,31 @@
 
 var database = firebase.database();
 var databaseRef = database.ref("/");
-{/* <div class="col-lg-4 col-sm-6 portfolio-item">
-              <div class="card h-100">
-                <div class="card-body">
-                  <h4 class="card-title">
-                    <a href="#">Project One</a>
-                  </h4>
-                  <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
-                </div>
-              </div>
-            </div> */}
+
 // Read the data from the database and take a snapshot of that data.
+
 databaseRef.once("value").then(function(snapshot) {
  // Use .val() to get the data from the snapshot.
  const directory = snapshot.val();
  console.log(directory);
+ var UName;
+ var UPass;
+ 
     for(var key in directory){
         console.log(key);
         console.log(directory[key].password);
-        var UName = directory[key].username;
-        var UPass = directory[key].password;
+        UName = directory[key].username;
+        UPass = directory[key].password;
         
-        $("#login").click(function(){
-        if(UName === $("#username").val() && UPass === $("#password").val() ){
+        
+    }
+    $("#login").click(function(){
+        if(UName === $("#username").val() && UPass === $("#password").val()){
             window.location.replace("blog.html");
+            
+        } else {
+            $("#incorrect").html("That was incorrect");
         }
         
         });
-    }
 });
